bb70a83c2060b366195defc1e60650fdd678558a
Lucas Hafner <lucasezequielhafner@gmail.com>
2025-07-23 23:53:06 +0200

    add custom validations

--- Models/AccountType.cs
+++ Models/AccountType.cs
@@ -1,12 +1,14 @@
 using System.ComponentModel.DataAnnotations;
+using BudgetManagement.Validations;
 
 namespace BudgetManagement.Models
 {
-    public class Accounttype
+    public class AccountType
     {
         public int Id { get; set; }
         [Required(ErrorMessage = "El campo nombre es requerido")]
-        [StringLength(maximumLength : 50, MinimumLength = 3, ErrorMessage = "El campo nombre debe tener un mínimo de {2} caracteres y un máximo de {1}")]
+        [StringLength(maximumLength: 50, MinimumLength = 3, ErrorMessage = "El campo nombre debe tener un mínimo de {2} caracteres y un máximo de {1}")]
+        [CapitalizedFirstLetter]
         public string Name { get; set; }
         public int UserId { get; set; }
         public int Order { get; set; }
